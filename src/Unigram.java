0b8cad15197122d568efed999199f860225d92ed
Sarah Brown <esbie.is@gmail.com>
2009-03-29 20:56:51 -0700

    first implementation of perplexity for unigrams and bigrams

--- src/Unigram.java
+++ src/Unigram.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Stack;
 
 public class Unigram
 {
@@ -14,13 +15,17 @@ public class Unigram
     
     public static void main(String[] args)
     {
-		NgramParser p = new NgramParser("data/fbistest.xml");    	
+		NgramParser p = new NgramParser("data/fbistrain.xml");
         HashSet<String> set = p.parse();
         Unigram u = new Unigram(set);
         u.train();
-        //u.showCounts();
+        // u.showCounts();
         System.out.println("P(year) = " + u.unsmoothedProbability("year"));
         System.out.println(u.getSentence());
+
+        NgramParser test = new NgramParser("data/fbistest.xml");
+        HashSet<String> testset = test.parse();
+        System.out.println(u.perplexity(testset));
     }
     
     public Unigram(Set<String> samples)
@@ -71,9 +76,14 @@ public class Unigram
         String sentence = "";
         String currentWord = START;
         Set<String> keySet = counts.keySet();
+        //creates a sentence until a period is found
+        //(400 is jic it doesn't find a period)
         while (!currentWord.equals(".") && sentence.length() <= 400) {
+            // rand is like a random dart thrown onto a dart board
+            // multiplied by totalCount for precision (since P(word) is small)
             double rand = Math.random() * totalCount;
             Iterator<String> i = keySet.iterator();
+            //looking at all the words to see where the dart lands
             while (i.hasNext() && rand >= 0) {
                 currentWord = i.next();
                 rand -= (double) counts.get(currentWord);
@@ -81,6 +91,35 @@ public class Unigram
             sentence += currentWord + " ";
         }
         return sentence;
+    }
+
+    public double perplexity(Set<String> testSamples) {
+        float product = 1;
+        int wordCount = 0;
+        Stack<Double> products = new Stack<Double>();
+        String regexp = "('?\\w+|\\p{Punct})";
+        Pattern pattern = Pattern.compile(regexp);
+
+        // counting number of words in test set
+        for (String sample : testSamples) {
+            Matcher matcher = pattern.matcher(sample);
+            while (matcher.find()) {
+                String match = matcher.group();
+                if (unsmoothedProbability(match) > 0) {
+                    products.push(unsmoothedProbability(match));
+                    wordCount++;
+                }
+            }
+        }
+
+        // computing the necessary exponent
+        double power = 1.0 / wordCount;
+
+        // computing perplexity based on probabilities
+        while (!products.empty()) {
+            product *= Math.pow(products.pop(), power);
+        }
+        return 1 / product;
     }
 
 }
