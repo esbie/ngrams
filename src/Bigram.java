b6886f81d8a3d67db697c5d7d8995ba7db10f185
Sarah Brown <esbie.is@gmail.com>
2009-03-31 23:13:02 -0400

    a whole mess of smoothing. Only simpleSmoothing works for Bigrams

--- src/Bigram.java
+++ src/Bigram.java
@@ -1,5 +1,8 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -9,8 +12,8 @@ import java.util.Stack;
 public class Bigram
 {
     public Set<String> samples;
-    public HashMap<String, HashMap<String, Integer>> counts;
-    public HashMap<String, Integer> unigramCounts;
+    public HashMap<String, HashMap<String, Double>> counts;
+    public HashMap<String, Double> unigramCounts;
     public final String START = ":S";
     
     public static void main(String[] args)
@@ -23,17 +26,20 @@ public class Bigram
         System.out.println("P(a year) = "
                 + b.unsmoothedProbability("a", "year"));
         System.out.println(b.getSentence());
+        //b.simpleSmoothing();
+        System.out.println("P(a year) = "
+                + b.unsmoothedProbability("a", "year"));
 
         NgramParser test = new NgramParser("data/fbistest.xml");
         HashSet<String> testset = test.parse();
-        System.out.println(b.perplexity(testset));
+        System.out.println(b.perplexity2(testset));
     }
     
     public Bigram(Set<String> samples)
     {
         this.samples = samples;
-        this.counts = new HashMap<String, HashMap<String, Integer>>();
-        this.unigramCounts = new HashMap<String, Integer>();
+        this.counts = new HashMap<String, HashMap<String, Double>>();
+        this.unigramCounts = new HashMap<String, Double>();
     }
     
     public void train()
@@ -46,7 +52,7 @@ public class Bigram
             String previousWord = START; // originally set to beginning-of-sentence marker
             while (matcher.find()) {
                 // Set unigram counts (for word1)
-                int unigramCount = 0;
+                double unigramCount = 0;
                 if (unigramCounts.containsKey(previousWord)) {
                     unigramCount = unigramCounts.get(previousWord);
                 }
@@ -56,16 +62,16 @@ public class Bigram
                 String match = matcher.group();
                 
                 // Get access to (or create) the count map for word1.
-                HashMap<String, Integer> innerCounts;
+                HashMap<String, Double> innerCounts;
                 if (counts.containsKey(previousWord)) {
                     innerCounts = counts.get(previousWord);
                 } else {
-                    innerCounts = new HashMap<String, Integer>();
+                    innerCounts = new HashMap<String, Double>();
                     counts.put(previousWord, innerCounts);
                 }
                 
                 // Set bigram counts
-                int count = 0;
+                double count = 0;
                 if (innerCounts.containsKey(match)) {
                     count = innerCounts.get(match);
                 }
@@ -121,6 +127,107 @@ public class Bigram
         }
         return sentence;
     }
+    
+    public double perplexity2(Set<String> testSamples) {
+        float product = 1;
+        int wordCount = 0;
+        String regexp = "('?\\w+|\\p{Punct})";
+        Pattern pattern = Pattern.compile(regexp);
+
+        int j = 0;
+        // counting number of words in test set
+        // adding previously unseen words/bigrams to the hashmap with count 0
+        for (String sample : testSamples) {
+            Matcher matcher = pattern.matcher(sample);
+            String previousWord = START;
+            while (matcher.find()) {
+                String match = matcher.group();
+                wordCount++;
+                if (counts.containsKey(previousWord)) {
+                    if (!counts.get(previousWord).containsKey(match)) {
+                        counts.get(previousWord).put(match, 0.0);
+                    }
+                } else {
+                    HashMap<String, Double> innerMap = new HashMap<String, Double>();
+                    innerMap.put(match, 0.0);
+                    counts.put(previousWord, innerMap);
+                    unigramCounts.put(previousWord, 0.0);
+                }
+                // Update previousWord
+                previousWord = match;
+            }
+            j++;
+            System.out.println(j+" of "+testSamples.size()+" iterations");
+        }
+
+        // computing the necessary exponent
+        double power = 1.0 / wordCount;
+        System.out.println("now smoothing");        
+        simpleSmoothing();
+        System.out.println("now computing perplexity");
+        // computing perplexity based on smoothed probabilities
+        for (String sample : testSamples) {
+            Matcher matcher = pattern.matcher(sample);
+            String previousWord = START;
+            while (matcher.find()) {
+                String match = matcher.group();
+                product *= Math.pow(unsmoothedProbability(previousWord, match), power);
+                // Update previousWord
+                previousWord = match;
+            }
+        }
+        return 1 / product;
+    }
+    
+    public void simpleSmoothingBroken(){
+        int j = 0;
+        Collection<String> keySet = counts.keySet();
+        for(String word1 : keySet){
+            System.out.println("word1 "+word1);
+            if (!counts.containsKey(word1)) {
+                System.out.println("created new Map");
+                counts.put(word1, new HashMap<String,Double>());
+            }
+            if(!unigramCounts.containsKey(word1)){
+                System.out.println("created new unigram count");
+                unigramCounts.put(word1, 0.0);
+            }
+            Double totalCount = unigramCounts.get(word1);
+            for(String word2 : keySet){
+                System.out.println("    word1 "+word1+" word2 "+word2);
+                if (!counts.get(word1).containsKey(word2)) {
+                    counts.get(word1).put(word2, 0.0);                        
+                }
+                
+                Double count = counts.get(word1).get(word2);
+                System.out.println("checkpoint");
+                count++;
+                totalCount++;
+                System.out.println("checkpoint2");
+                counts.get(word1).put(word2, count);
+                System.out.println("    count "+count+" totalCount "+totalCount);
+            }
+            unigramCounts.put(word1, totalCount);
+            j++;
+            System.out.println(j+" of "+keySet.size()+" smoothing iterations");
+        }
+    }
+    
+    public void simpleSmoothing(){
+        Collection<String> keySet = counts.keySet();
+        for(String word1 : keySet){
+            if (counts.containsKey(word1) && unigramCounts.containsKey(word1)) {
+                Double totalCount = unigramCounts.get(word1);
+                for(String word2 : counts.get(word1).keySet()){
+                    Double count = counts.get(word1).get(word2);
+                    count++;
+                    totalCount++;
+                    counts.get(word1).put(word2, count);
+                }
+                unigramCounts.put(word1, totalCount);
+            }
+        }
+    }
 
     public double perplexity(Set<String> testSamples) {
         float product = 1;
@@ -152,5 +259,56 @@ public class Bigram
             product *= Math.pow(products.pop(), power);
         }
         return 1 / product;
+    }
+    
+    public void smoothing(){
+        Collection<HashMap<String, Double>> collection = counts.values();
+        HashMap<Double, Integer> map = new HashMap<Double, Integer>();
+        //counting how many ngrams appear c times for each double c in the counts collection
+        for(HashMap<String, Double> keymap: collection){
+            for(Double key : keymap.values()){
+                if(map.containsKey(key)){
+                    int value = map.get(key);
+                    value++;
+                    map.put(key, value);
+                } else {
+                    map.put(key, 1);
+                }
+            }
+        }
+        HashMap<Double, Double> newcounts = new HashMap<Double, Double>();
+        ArrayList<Double> keys = new ArrayList<Double>();
+        //creates a sorted list with no duplicates
+        for (HashMap<String, Double> h : collection){
+            Collection<Double> noDups = new HashSet<Double>(h.values());
+            keys.addAll(noDups);
+        }
+        
+        Collections.sort(keys);
+        //creating lookup table for adjusted counts
+        for(int i=0; i<keys.size(); i++){
+            Double key = keys.get(i);
+            //check: does keys[i+1] exist
+            if(i != (keys.size() - 1)){
+                Double key2 = keys.get(i+1);
+                //number of ngrams that occur c times
+                int c = map.get(key);
+                //number of ngrams that occur c+1 times
+                int cplus1 = map.get(key2);
+                //this is the actual computation of good-turing smoothing
+                newcounts.put(key, key2*((double)cplus1/c));
+            }
+            else{
+                newcounts.put(key, key);
+            }
+        }
+
+        //switching out old counts for new counts
+        for(String key : counts.keySet()){
+            for(String key2 : counts.get(key).keySet()){
+                Double count = counts.get(key).get(key2);
+                counts.get(key).put(key2, newcounts.get(count));      
+            }
+        }
     }
 }
--- src/Unigram.java
+++ src/Unigram.java
@@ -1,15 +1,17 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.HashSet;
-import java.util.Stack;
 
 public class Unigram
 {
     public Set<String> samples;
-    public HashMap<String, Integer> counts;
+    public HashMap<String, Double> counts;
     public int totalCount;
     public final String START = ":S";
     
@@ -19,9 +21,11 @@ public class Unigram
         HashSet<String> set = p.parse();
         Unigram u = new Unigram(set);
         u.train();
-        // u.showCounts();
+        //u.showCounts();
+        //u.smoothing();
+        //u.showCounts();
         System.out.println("P(year) = " + u.unsmoothedProbability("year"));
-        System.out.println(u.getSentence());
+        //System.out.println(u.getSentence());
 
         NgramParser test = new NgramParser("data/fbistest.xml");
         HashSet<String> testset = test.parse();
@@ -31,7 +35,7 @@ public class Unigram
     public Unigram(Set<String> samples)
     {
         this.samples = samples;
-        this.counts = new HashMap<String, Integer>();
+        this.counts = new HashMap<String, Double>();
         this.totalCount = 0;
     }
     
@@ -45,7 +49,7 @@ public class Unigram
             while (matcher.find()) {
                 String match = matcher.group();
                 
-                int count = 0;
+                double count = 0;
                 if (counts.containsKey(match)) {
                     count = counts.get(match);
                 }
@@ -96,7 +100,6 @@ public class Unigram
     public double perplexity(Set<String> testSamples) {
         float product = 1;
         int wordCount = 0;
-        Stack<Double> products = new Stack<Double>();
         String regexp = "('?\\w+|\\p{Punct})";
         Pattern pattern = Pattern.compile(regexp);
 
@@ -105,21 +108,82 @@ public class Unigram
             Matcher matcher = pattern.matcher(sample);
             while (matcher.find()) {
                 String match = matcher.group();
-                if (unsmoothedProbability(match) > 0) {
-                    products.push(unsmoothedProbability(match));
-                    wordCount++;
+                wordCount++;
+                if (unsmoothedProbability(match) == 0.0) {
+                    counts.put(match, 0.0);
                 }
             }
         }
 
         // computing the necessary exponent
         double power = 1.0 / wordCount;
+        
+        simplesmoothing();
 
-        // computing perplexity based on probabilities
-        while (!products.empty()) {
-            product *= Math.pow(products.pop(), power);
+        // computing perplexity based on smoothed probabilities
+        for (String sample : testSamples) {
+            Matcher matcher = pattern.matcher(sample);
+            while (matcher.find()) {
+                String match = matcher.group();
+                product *= Math.pow(unsmoothedProbability(match), power);
+            }
         }
         return 1 / product;
+    }
+    
+    public void simplesmoothing(){
+        for(String key : counts.keySet()){
+            Double count = counts.get(key);
+            count++;
+            totalCount++;
+            counts.put(key, count);
+        }
+    }
+    
+    public void smoothing(){
+        Collection<Double> collection = counts.values();
+        HashMap<Double, Integer> map = new HashMap<Double, Integer>();
+        //counting how many ngrams appear c times for each double c in the counts collection
+        for(Double key: collection){
+            if(map.containsKey(key)){
+                int value = map.get(key);
+                value++;
+                map.put(key, value);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        HashMap<Double, Double> newcounts = new HashMap<Double, Double>();
+        
+        //creates a sorted list with no duplicates
+        Collection<Double> noDups = new HashSet<Double>(collection);
+        ArrayList<Double> keys = new ArrayList<Double>();
+        keys.addAll(noDups);
+        Collections.sort(keys);
+        //creating lookup table for adjusted counts
+        for(int i=0; i<keys.size(); i++){
+            Double key = keys.get(i);
+            //check: does keys[i+1] exist
+            if(i != (keys.size() - 1)){
+                Double key2 = keys.get(i+1);
+                //number of ngrams that occur c times
+                int c = map.get(key);
+                //number of ngrams that occur c+1 times
+                int cplus1 = map.get(key2);
+                //this is the actual computation of good-turing smoothing
+                //TODO: use logs for precision
+                newcounts.put(key, key2*((double)cplus1/c));
+            }
+            else{
+                newcounts.put(key, key);
+            }
+        }
+
+        //switching out old counts for new counts
+        for(String key : counts.keySet()){
+            Double count = counts.get(key);
+            counts.put(key, newcounts.get(count));            
+        }
     }
 
 }
