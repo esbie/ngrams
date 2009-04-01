4b38ebcd41d8f1349f4b59200f60048a8b4aa92a
Rudd Zwolinski <ruddzw@gmail.com>
2009-04-01 17:57:59 -0400

    Unigrams work with add-one, and good-turing smoothing

--- src/Unigram.java
+++ src/Unigram.java
@@ -9,30 +9,51 @@ import java.util.Stack;
 public class Unigram
 {
     public Set<String> samples;
-    public HashMap<String, Integer> counts;
-    public int totalCount;
+    public HashMap<String, Double> counts;
+    public double totalCount;
     public final String START = ":S";
+    
+    // For add-one smoothing
+    public Set<String> wordSet; // Used to find the vocabulary
+    public double vocabSize; // Size of the vocabulary
+    
+    // For Good Turing Smoothing
+    public double numTrainingUnigrams; // The size of the training set (# non-distinct words)
+    public HashMap<Double, Double> numberOfUnigramsWithCount; // The number of unigrams that occur x times
+    public boolean goodTuringCountsAvailable = false; // True when good turing counts are available
     
     public static void main(String[] args)
     {
-		NgramParser p = new NgramParser("data/fbistrain.xml");
+		if (args.length != 2) {
+            System.out.println("You must supply 2 arguments:\n(1) Training file\n" +
+                               "(2) Test file");
+            System.exit(1);
+        }
+        
+		NgramParser p = new NgramParser(args[0], true);
         HashSet<String> set = p.parse();
+        
         Unigram u = new Unigram(set);
         u.train();
-        // u.showCounts();
-        System.out.println("P(year) = " + u.unsmoothedProbability("year"));
-        System.out.println(u.getSentence());
+        
+        System.out.println("Done training.");
 
-        NgramParser test = new NgramParser("data/fbistest.xml");
+        //System.out.println(u.getSentence());
+        
+        NgramParser test = new NgramParser(args[1], true);
         HashSet<String> testset = test.parse();
-        System.out.println(u.perplexity(testset));
+        System.out.println("Perplexity of the test set: " + u.perplexity(testset));
     }
     
     public Unigram(Set<String> samples)
     {
         this.samples = samples;
-        this.counts = new HashMap<String, Integer>();
+        this.counts = new HashMap<String, Double>();
         this.totalCount = 0;
+        
+        this.wordSet = new HashSet<String>();
+        
+        this.numberOfUnigramsWithCount = new HashMap<Double, Double>();
     }
     
     public void train()
@@ -45,24 +66,82 @@ public class Unigram
             while (matcher.find()) {
                 String match = matcher.group();
                 
-                int count = 0;
+                wordSet.add(match);
+                
+                double count = 0;
                 if (counts.containsKey(match)) {
                     count = counts.get(match);
+                    numberOfUnigramsWithCount.put(count, numberOfUnigramsWithCount.get(count)-1);
                 }
                 counts.put(match, count+1);
+                if (!numberOfUnigramsWithCount.containsKey(count+1)) {
+                    numberOfUnigramsWithCount.put(count+1, 1.0);
+                } else {
+                    numberOfUnigramsWithCount.put(count+1, numberOfUnigramsWithCount.get(count+1)+1);
+                }
                 
                 totalCount++;
+                numTrainingUnigrams++;
             }
         }
+        
+        vocabSize = wordSet.size();
     }
     
     public double unsmoothedProbability(String word)
     {
         if (counts.containsKey(word)) {
-            return (double)counts.get(word) / (double) totalCount;
+            return counts.get(word) / totalCount;
         } else {
             return 0.0;
         }
+    }
+    
+    public double count(String word)
+    {
+        if (counts.containsKey(word)) {
+            return counts.get(word);
+        }
+        return 0.0;
+    }
+    
+    public double addOneSmoothedProbability(String word)
+    {
+        return (count(word) + 1.0) / (totalCount + vocabSize);
+    }
+    
+    public double goodTuringSmoothedProbability(String word)
+    {
+        if (!goodTuringCountsAvailable) {
+            System.out.println("Making good turing counts...");
+            makeGoodTuringCounts();
+            System.out.println("Done making good turing counts.");
+        }
+        
+        // If this unigram has occurred, return good turing probability
+        double gtcount = count(word);
+        if (gtcount > 0.0) {
+            return gtcount / totalCount;
+        }
+        // Otherwise, return N1/N as per book (page 101?)
+        return numberOfUnigramsWithCount.get(1.0) / numTrainingUnigrams;
+    }
+    
+    public void makeGoodTuringCounts()
+    {
+        // Generate good turing counts
+        totalCount = 0;
+        for (String word : counts.keySet()) {
+            double count = counts.get(word);
+            if (!numberOfUnigramsWithCount.containsKey(count+1)) {
+                numberOfUnigramsWithCount.put(count+1, 0.0);
+            }
+            // c* = (c+1) * N(c+1) / N(c)
+            double newCount = (count + 1)*(numberOfUnigramsWithCount.get(count+1.0))/(numberOfUnigramsWithCount.get(count));
+            counts.put(word, newCount);
+            totalCount += newCount;
+        }
+        goodTuringCountsAvailable = true;
     }
     
     public void showCounts()
@@ -86,7 +165,7 @@ public class Unigram
             //looking at all the words to see where the dart lands
             while (i.hasNext() && rand >= 0) {
                 currentWord = i.next();
-                rand -= (double) counts.get(currentWord);
+                rand -= counts.get(currentWord);
             }
             sentence += currentWord + " ";
         }
@@ -105,10 +184,12 @@ public class Unigram
             Matcher matcher = pattern.matcher(sample);
             while (matcher.find()) {
                 String match = matcher.group();
-                if (unsmoothedProbability(match) > 0) {
+                /*if (unsmoothedProbability(match) > 0) {
                     products.push(unsmoothedProbability(match));
                     wordCount++;
-                }
+                }*/
+                products.push(goodTuringSmoothedProbability(match));
+                wordCount++;
             }
         }
 
